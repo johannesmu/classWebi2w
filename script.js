3132cfdd6707a4ffb9926b71b42d1c445f46ba3f
johannes <jmuljana@gmail.com>
2020-09-28 11:14:43 +1000

    added dropdown menu

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function onLoad() {
   const btn = document.querySelector('.navbutton')
   const nav = document.querySelector('.navigation')
+  const dropdown = document.querySelector('.dropdown')
   // make btn respond to a click
   btn.addEventListener('click', function() {
     if( nav.classList.contains('open') ){
@@ -8,6 +9,15 @@ function onLoad() {
     }
     else {
       nav.classList.add('open')
+    }
+  })
+  // dropdown
+  dropdown.addEventListener('click', function(evt) {
+    if( dropdown.classList.contains('open') ) {
+      dropdown.classList.remove('open')
+    }
+    else {
+      dropdown.classList.add('open')
     }
   })
 }
